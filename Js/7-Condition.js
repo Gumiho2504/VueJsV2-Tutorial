bfd5a5f655eb3bdc95b3087eb3e81d9dd7c92aca
GumiHo <hemchanmetrey123@gmail.com>
2022-11-04 10:43:54 +0700

    YA

---
+++ Js/7-Condition.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#app',
+    data: {
+        alertError: {
+            color: 'green',
+            fontSize: '30px',
+            fontWeight: 'bold'
+        },
+        alertSuccess:{
+            color: 'blue',
+            fontSize: '60px',
+            fontWeight: 'bold'
+        },
+    success: false,
+    error: false
+    },
+    
+    methods: {
+       
+    }
+})
---
+++ Js/8-Loop-VueJs.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: '#app',
+    data: {
+        object: {
+            title: 'How to do list in Vue',
+            author:'GuMiho',
+            publisheAt: '2016-04-10'
+        },
+        person: [
+            {name: 'Gumiho' , age: 22},
+            {name: 'Hong' , age: 18},
+            {name: 'Ma Ya' , age: 32},
+            {name: 'Ly Yun' , age: 22},
+            {name: 'Kim soohyon' , age: 35},
+            {name: 'Ji Changwook' , age: 35},
+        ]
+    },
+    
+    methods: {
+       
+    }
+})
